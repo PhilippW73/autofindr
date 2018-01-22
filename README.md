e32d519870fce27160b51bb182404d85c5ce475b
Philipp Wickart <phwickart@gmail.com>
2018-01-22 18:54:22 -0500

    udpate

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # autofindr
+
+##Technologies Used
+* Node.js/NPM
+* Express.js
+* Heroku
+* Bootstrap
+
+##Setup
+* The survey on the autofindr homepage should have 10 questions. Each answer will be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+
+* Determine the user's most compatible car using the following as a guide:
+
+* Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+With that done, compare the difference between current user's scores against existing cars, question by question. Add up the differences to calculate the totalDifference.
+
+Example:
+User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+Car 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+Total Difference: 2 + 1 + 2 = 5
+
+Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+The closest match will be the car that fits the user the best.
+Once you've found the current user's most compatible car, display the result as a modal pop-up.
+
+* The modal should display both the name and picture of the closest match.
+
--- data/car.js
+++ data/car.js
@@ -2,27 +2,27 @@
 var cars = [
   {
     name: "Pedalo for Adults",
-    photo: "https://dummyimage.com/300x200/042/fff",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuKPKqM2DwSaJtQ21IO8HimolZ0wACesGo17XpOLHkKxndf_wang",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
-    name: "Ugly Green Thing",
-    photo:"https://dummyimage.com/300x200/042/fff",
+    name: "Also ugly drives",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJHhKBS4KyAXNgsqmxGiCuJTziEPDXQ3Wg1aYA-WbQgDv0O3FjgQ",
     scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   },
   {
     name: "Drives you everywhere",
-    photo:"https://dummyimage.com/300x200/042/fff",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNZ2QP5snJYZNFMguWdxkzzJHVdzNEhmrkKlLC1_bg50dsROVKf",
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: "Got some power",
-    photo:"https://dummyimage.com/300x200/042/fff",
+    photo:"http://cdn-ds.com/media/2187/w_1920/2018-Dodge-Challenger-SRT-Demon-Static-26-2880x1800.jpg",
     scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
   },
   {
     name: "Be a superhero",
-    photo:"https://dummyimage.com/300x200/042/fff",
+    photo:"https://static.turbosquid.com/Preview/2016/05/04__14_03_32/BatmobileTumbler6.jpg91094a94-642e-484f-9f96-1cd10c135206Original.jpg",
     scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   },
   
